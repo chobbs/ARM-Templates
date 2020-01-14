46b9ebce3064d46887a5fecc218118bf6f298335
chobbs <craig.hobbs@gmail.com>
2020-01-13 16:21:00 -0800

    updates

--- src/scripts/datanodes-configure.sh
+++ src/scripts/datanodes-configure.sh
@@ -1,29 +1,11 @@
 #!/bin/bash
 
-# The MIT License (MIT)
+# License: https://github.com/influxdata/azure-resource-manager-influxdb-enterprise/blob/master/LICENSE
 #
-# Copyright (c) 2020 Microsoft Azure
+# Configure InfluxEnterprise from deployed ARM templates 
+# Initial Version
 #
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-#
-# Craig Hobbs (Partner Engineer, InfluxData)
-#
+
 help()
 {
     echo "This script finishes the datanode configuration for the  InfluxEnterprise cluster on Ubuntu"
@@ -49,15 +31,22 @@ help()
     echo "-h view this help content"
 }
 
-# Log method to control/redirect log output
+#########################
+# Logging func
+#########################
+
+# Custom logging with time so we can easily relate running times, also log to separate file so order is guaranteed.
+# The Script extension output the stdout/err buffer in intervals with duplicates.
+
 log()
 {
-    # If you want to enable this logging add a un-comment the line below and add your account id
-    #curl -X POST -H "content-type:text/plain" --data-binary "${HOSTNAME} - $1" https://logs-01.loggly.com/inputs/<key>/tag/es-extension,${HOSTNAME}
-    echo "$1"
+
+     echo \[$(date +%d%m%Y-%H:%M:%S)\] "$1"
+     echo \[$(date +%d%m%Y-%H:%M:%S)\] "$1" >> /var/log/cluster-configuration.log
 }
 
-log "Begin execution of Elasticsearch script extension on ${HOSTNAME}"
+
+log "Begin execution of Cluster Configuration script extension on ${HOSTNAME}"
 
 if [ "${UID}" -ne 0 ];
 then
@@ -66,119 +55,69 @@ then
     exit 3
 fi
 
-# TEMP FIX - Re-evaluate and remove when possible
-# This is an interim fix for hostname resolution in current VM
-grep -q "${HOSTNAME}" /etc/hosts
-if [ $? == 0 ]
-then
-  echo "${HOSTNAME}found in /etc/hosts"
-else
-  echo "${HOSTNAME} not found in /etc/hosts"
-  # Append it to the hsots file if not there
-  echo "127.0.0.1 ${HOSTNAME}" >> /etc/hosts
-  log "hostname ${HOSTNAME} added to /etchosts"
-fi
+#########################
+# Configuration functions
+#########################
+
+setup_metanodes()
+{
+
+  grep -q "${HOSTNAME}" /etc/hosts
+  if [ $? == 0 ]
+  then
+    echo "${HOSTNAME} found in /etc/hosts skip adding"
+  else
+    echo "Adding metanode-vm host in /etc/hosts"
+
+    # TEMP-FIX: Hard coded privateIP's 
+    # Append metanode vm hostname  to the hsots file
+    echo "10.0.0.10 metanode-vm0" >> /etc/hosts
+    echo "10.0.0.11 metanode-vm1" >> /etc/hosts
+    echo "10.0.0.12 metanode-vm2" >> /etc/hosts
+
+    log "hostnames added to /etc/hosts"
+  fi
+}
+
+
+config_file()
+{
+
+  echo $HOSTNAME | awk '{print substr($0,0,8)}'
+  if [ $? == datanode ]
+  then
+   #Generate new confige fiel
+    influxd config > "${CONFIG_FILE}"
+    sed -i "s/\(hostname *= *\).*/\1\"$HOSTNAME\"/" "${CONFIG_FILE}"
+    sed -i "s/\(license-key *= *\).*/\1\"$TEMP_LICENSE\"/" "${CONFIG_FILE}"
+
+    mkdir -p "/influxdb/data"
+    mkdir -p "/influxdb/meta"
+    mkdir -p "/influxdb/wal"
+    chown -R influxdb:influxdb "/influxdb/"
+    log "Configuration completed"
+  fi
+}
+
+
+start_systemd()
+{
+    log "[start_systemd] starting Metanode"
+    sudo systemctl start influxdb-meta
+    log "[start_systemd] started Metanode"
+}
+
 
 #Script Parameters
-CLUSTER_NAME="elasticsearch"
+CONFIG_FILE=/etc/influxdb/influxdb.generated.conf
+TEMP_LICENSE="d2951f76-a329-4bd9-b9bc-12984b897031"
+
 ES_VERSION="2.0.0"
 INSTALL_PLUGINS=0
 CLIENT_ONLY_NODE=0
 DATA_NODE=0
 MASTER_ONLY_NODE=0
 
-CLUSTER_USES_DEDICATED_MASTERS=0
-DATANODE_COUNT=0
-
-MINIMUM_MASTER_NODES=3
-UNICAST_HOSTS='["masterVm0:9300","masterVm1:9300","masterVm2:9300"]'
-
-USER_ADMIN_PWD="changeME"
-USER_READ_PWD="changeME"
-USER_KIBANA4_PWD="changeME"
-USER_KIBANA4_SERVER_PWD="changeME"
-
-#Loop through options passed
-while getopts :n:v:A:R:K:S:Z:xyzldh optname; do
-  log "Option $optname set"
-  case $optname in
-    n) #set cluster name
-      CLUSTER_NAME=${OPTARG}
-      ;;
-    v) #elasticsearch version number
-      ES_VERSION=${OPTARG}
-      ;;
-    A) #shield admin pwd
-      USER_ADMIN_PWD=${OPTARG}
-      ;;
-    R) #shield readonly pwd
-      USER_READ_PWD=${OPTARG}
-      ;;
-    K) #shield kibana user pwd
-      USER_KIBANA4_PWD=${OPTARG}
-      ;;
-    S) #shield kibana server pwd
-      USER_KIBANA4_SERVER_PWD=${OPTARG}
-      ;;
-    Z) #number of data nodes hints (used to calculate minimum master nodes)
-      DATANODE_COUNT=${OPTARG}
-      ;;
-    x) #master node
-      MASTER_ONLY_NODE=1
-      ;;
-    y) #client node
-      CLIENT_ONLY_NODE=1
-      ;;
-    z) #client node
-      DATA_NODE=1
-      ;;
-    l) #install plugins
-      INSTALL_PLUGINS=1
-      ;;
-    d) #cluster is using dedicated master nodes
-      CLUSTER_USES_DEDICATED_MASTERS=1
-      ;;
-    h) #show help
-      help
-      exit 2
-      ;;
-    \?) #unrecognized option - show help
-      echo -e \\n"Option -${BOLD}$OPTARG${NORM} not allowed."
-      help
-      exit 2
-      ;;
-  esac
-done
-
-if [ ${CLUSTER_USES_DEDICATED_MASTERS} -ne 0 ]; then
-    MINIMUM_MASTER_NODES=2
-    UNICAST_HOSTS='["masterVm0:9300","masterVm1:9300","masterVm2:9300"]'
-else
-    MINIMUM_MASTER_NODES=$(((DATANODE_COUNT/2)+1))
-    UNICAST_HOSTS='['
-    for i in $(seq 0 $((DATANODE_COUNT-1))); do
-        UNICAST_HOSTS="$UNICAST_HOSTS\"esdatavm$i:9300\","
-    done
-    UNICAST_HOSTS="${UNICAST_HOSTS%?}]"
-fi
-
-log "Bootstrapping an Elasticsearch $ES_VERSION cluster named '$CLUSTER_NAME' with minimum_master_nodes set to $MINIMUM_MASTER_NODES"
-log "Cluster uses dedicated master nodes is set to $CLUSTER_USES_DEDICATED_MASTERS and unicast goes to $UNICAST_HOSTS"
-log "Cluster install script is set to $INSTALL_PLUGIN"
-
-# Base path for data disk mount points
-# The script assume format /datadisks/disk1 /datadisks/disk2
-DATA_BASE="/datadisks"
-
-# Configure Elasticsearch Data Disk Folder and Permissions
-setup_data_disk()
-{
-    log "Configuring disk $1/elasticsearch/data"
-
-    mkdir -p "$1/elasticsearch/data"
-    chown -R elasticsearch:elasticsearch "$1/elasticsearch"
-    chmod 755 "$1/elasticsearch"
-}
 
 # Install Oracle Java
 install_java()
@@ -213,7 +152,7 @@ install_ntp()
 {
     log "installing ntp deamon"
     apt-get -y install ntp
-    ntpdate pool.ntp.org
+    #ntpdate pool.ntp.org
     log "installed ntp deamon and ntpdate"
 }
 
@@ -225,16 +164,22 @@ install_ntp
 #Format data disks (Find data disks then partition, format, and mount them as seperate drives)
 # using the -s paramater causing disks under /datadisks/* to be raid0'ed
 #------------------------
-bash vm-disk-utils-0.1.sh -s
+# bash vm-disk-utils-0.1.sh -s
+
 
 #Install Oracle Java
 #------------------------
-install_java
+setup_hostnames
+
+
+#Install Oracle Java
+#------------------------
+#install_java
 
 #
 #Install Elasticsearch
 #-----------------------
-install_es
+#install_es
 
 # Prepare configuration information
 # Configure permissions on data disks for elasticsearch user:group
@@ -242,165 +187,4 @@ install_es
 RAIDDISK="/datadisks/disk1"
 DATAPATH_CONFIG="/datadisks/disk1/elasticsearch/data"
 
-setup_data_disk ${RAIDDISK}
-
-#if [ -d "${DATA_BASE}" ]; then
-#    for D in `find /datadisks/ -mindepth 1 -maxdepth 1 -type d`
-#    do
-#        #Configure disk permissions and folder for storage
-#        setup_data_disk ${D}
-#        # Add to list for elasticsearch configuration
-#        DATAPATH_CONFIG+="$D/elasticsearch/data,"
-#    done
-#    #Remove the extra trailing comma
-#    DATAPATH_CONFIG="${DATAPATH_CONFIG%?}"
-#else
-#    #If we do not find folders/disks in our data disk mount directory then use the defaults
-#    log "Configured data directory does not exist for ${HOSTNAME} using defaults"
-#fi
-
-#Configure Elasticsearch settings
-#---------------------------
-#Backup the current Elasticsearch configuration file
-mv /etc/elasticsearch/elasticsearch.yml /etc/elasticsearch/elasticsearch.bak
-
-# Set cluster and machine names - just use hostname for our node.name
-echo "cluster.name: $CLUSTER_NAME" >> /etc/elasticsearch/elasticsearch.yml
-echo "node.name: ${HOSTNAME}" >> /etc/elasticsearch/elasticsearch.yml
-
-# Configure paths - if we have data disks attached then use them
-if [ -n "$DATAPATH_CONFIG" ]; then
-    log "Update configuration with data path list of $DATAPATH_CONFIG"
-    echo "path.data: $DATAPATH_CONFIG" >> /etc/elasticsearch/elasticsearch.yml
-fi
-
-# Configure discovery
-log "Update configuration with hosts configuration of $UNICAST_HOSTS"
-echo "discovery.zen.ping.multicast.enabled: false" >> /etc/elasticsearch/elasticsearch.yml
-echo "discovery.zen.ping.unicast.hosts: $UNICAST_HOSTS" >> /etc/elasticsearch/elasticsearch.yml
-
-
-# Configure Elasticsearch node type
-log "Configure master/client/data node type flags master-$MASTER_ONLY_NODE data-$DATA_NODE"
-
-if [ ${MASTER_ONLY_NODE} -ne 0 ]; then
-    log "Configure node as master only"
-    echo "node.master: true" >> /etc/elasticsearch/elasticsearch.yml
-    echo "node.data: false" >> /etc/elasticsearch/elasticsearch.yml
-    # echo "marvel.agent.enabled: false" >> /etc/elasticsearch/elasticsearch.yml
-elif [ ${DATA_NODE} -ne 0 ]; then
-    log "Configure node as data only"
-    echo "node.master: false" >> /etc/elasticsearch/elasticsearch.yml
-    echo "node.data: true" >> /etc/elasticsearch/elasticsearch.yml
-    # echo "marvel.agent.enabled: false" >> /etc/elasticsearch/elasticsearch.yml
-elif [ ${CLIENT_ONLY_NODE} -ne 0 ]; then
-    log "Configure node as data only"
-    echo "node.master: false" >> /etc/elasticsearch/elasticsearch.yml
-    echo "node.data: false" >> /etc/elasticsearch/elasticsearch.yml
-    # echo "marvel.agent.enabled: false" >> /etc/elasticsearch/elasticsearch.yml
-else
-    log "Configure node for master and data"
-    echo "node.master: true" >> /etc/elasticsearch/elasticsearch.yml
-    echo "node.data: true" >> /etc/elasticsearch/elasticsearch.yml
-fi
-
-echo "discovery.zen.minimum_master_nodes: $MINIMUM_MASTER_NODES" >> /etc/elasticsearch/elasticsearch.yml
-echo "network.host: _non_loopback_" >> /etc/elasticsearch/elasticsearch.yml
-#echo "bootstrap.mlockall: true" >> /etc/elasticsearch/elasticsearch.yml
-
-# DNS Retry
-echo "options timeout:10 attempts:5" >> /etc/resolvconf/resolv.conf.d/head
-resolvconf -u
-
-# Increase maximum mmap count
-echo "vm.max_map_count = 262144" >> /etc/sysctl.conf
-
-#"action.disable_delete_all_indices: ${DISABLE_DELETE_ALL}" >> /etc/elasticsearch/elasticsearch.yml
-#"action.auto_create_index: ${AUTOCREATE_INDEX}" >> /etc/elasticsearch/elasticsearch.yml
-
-# Configure Environment
-#----------------------
-#/etc/default/elasticseach
-#Update HEAP Size in this configuration or in upstart service
-#Set Elasticsearch heap size to 50% of system memory
-#TODO: Move this to an init.d script so we can handle instance size increases
-ES_HEAP=`free -m |grep Mem | awk '{if ($2/2 >31744)  print 31744;else print $2/2;}'`
-log "Configure elasticsearch heap size - $ES_HEAP"
-echo "ES_HEAP_SIZE=${ES_HEAP}m" >> /etc/default/elasticsearch
-
-#Optionally Install Marvel
-log "Plugin install set to ${INSTALL_PLUGINS}"
-if [ ${INSTALL_PLUGINS} -ne 0 ]; then
-    log "Installing Plugins Shield, Marvel, Watcher"
-    sudo /usr/share/elasticsearch/bin/plugin install elasticsearch/license/latest
-    sudo /usr/share/elasticsearch/bin/plugin install elasticsearch/shield/latest
-    sudo /usr/share/elasticsearch/bin/plugin install elasticsearch/watcher/latest
-    sudo /usr/share/elasticsearch/bin/plugin install marvel-agent
-
-    log " Start adding es_admin"
-    sudo /usr/share/elasticsearch/bin/shield/esusers useradd "es_admin" -p "${USER_ADMIN_PWD}" -r admin
-    log " Finished adding es_admin"
-
-    log " Start adding es_read"
-    sudo /usr/share/elasticsearch/bin/shield/esusers useradd "es_read" -p "${USER_READ_PWD}" -r user
-    log " Finished adding es_read"
-
-    log " Start adding es_kibana"
-    sudo /usr/share/elasticsearch/bin/shield/esusers useradd "es_kibana" -p "${USER_KIBANA4_PWD}" -r kibana4
-    log " Finished adding es_kibina"
-
-    log " Start adding es_kibana_server"
-    sudo /usr/share/elasticsearch/bin/shield/esusers useradd "es_kibana_server" -p "${USER_KIBANA4_SERVER_PWD}" -r kibana4_server
-    log " Finished adding es_kibana_server"
-
-    echo "marvel.agent.enabled: true" >> /etc/elasticsearch/elasticsearch.yml
-fi
-
-
-#Install Monit
-#TODO - Install Monit to monitor the process (Although load balancer probes can accomplish this)
-apt-get -y install monit
-echo "set daemon 30" >> /etc/monit/monitrc
-echo "set httpd port 2812 and" >> /etc/monit/monitrc
-echo "    use address localhost" >> /etc/monit/monitrc
-echo "    allow localhost" >> /etc/monit/monitrc
-sudo touch /etc/monit/conf.d/elasticsearch.conf
-echo "check process elasticsearch with pidfile \"/var/run/elasticsearch/elasticsearch.pid\"" >> /etc/monit/conf.d/elasticsearch.conf
-echo "  group elasticsearch" >> /etc/monit/conf.d/elasticsearch.conf
-echo "  start program = \"/etc/init.d/elasticsearch start\"" >> /etc/monit/conf.d/elasticsearch.conf
-echo "  stop program = \"/etc/init.d/elasticsearch stop\"" >> /etc/monit/conf.d/elasticsearch.conf
-
-sudo /etc/init.d/monit start
-sudo monit start all
-
-#and... start the service
-log "Starting Elasticsearch on ${HOSTNAME}"
-update-rc.d elasticsearch defaults 95 10
-sudo service elasticsearch start
-log "complete elasticsearch setup and started"
-exit 0
-
-#Script Extras
-
-#Configure open file and memory limits
-#Swap is disabled by default in Ubuntu Azure VMs
-#echo "bootstrap.mlockall: true" >> /etc/elasticsearch/elasticsearch.yml
-
-# Verify this is necessary on azure
-#echo "elasticsearch    -    nofile    65536" >> /etc/security/limits.conf
-#echo "elasticsearch     -    memlock   unlimited" >> /etc/security/limits.conf
-#echo "session    required    pam_limits.so" >> /etc/pam.d/su
-#echo "session    required    pam_limits.so" >> /etc/pam.d/common-session
-#echo "session    required    pam_limits.so" >> /etc/pam.d/common-session-noninteractive
-#echo "session    required    pam_limits.so" >> /etc/pam.d/sudo
-
-#--------------- TEMP (We will use this for the update path yet) ---------------
-#Updating the properties in the existing configuraiton has been a bit sensitve and requires more testing
-#sed -i -e "/cluster\.name/s/^#//g;s/^\(cluster\.name\s*:\s*\).*\$/\1${CLUSTER_NAME}/" /etc/elasticsearch/elasticsearch.yml
-#sed -i -e "/bootstrap\.mlockall/s/^#//g;s/^\(bootstrap\.mlockall\s*:\s*\).*\$/\1true/" /etc/elasticsearch/elasticsearch.yml
-#sed -i -e "/path\.data/s/^#//g;s/^\(path\.data\s*:\s*\).*\$/\1${DATAPATH_CONFIG}/" /etc/elasticsearch/elasticsearch.yml
-
-# Minimum master nodes nodes/2+1 (These can be configured via API as well - (_cluster/settings))
-# gateway.expected_nodes: 10
-# gateway.recover_after_time: 5m
-#----------------------
+#setup_data_disk ${RAIDDISK}
